29f564d3e3af3b7fe0f4937f1b80bbce0304d88c
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-04-11 09:55:41 +0000

    Enhance documentation and fix pep8 requirements

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-This module provides a basic asynchronous coroutine named wait_random.
-It uses the random module to generate a float delay and asyncio to sleep.
+This module provides an asynchronous coroutine named wait_random.
+The goal of this module is to demonstrate the basic syntax of
+asynchronous programming in Python using the asyncio and random modules.
 """
 import asyncio
 import random
@@ -12,7 +13,7 @@ async def wait_random(max_delay: int = 10) -> float:
     Asynchronous coroutine that takes in an integer argument (max_delay,
     with a default value of 10) named wait_random that waits for a random
     delay between 0 and max_delay (included and float value) seconds
-    and eventually returns it.
+    and eventually returns it as a floating point number.
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
