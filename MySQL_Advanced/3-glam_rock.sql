c39ef64e15e847e62c28996a565715e08fe72593
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-05-02 17:57:05 +0400

    Revise Glam rock bands query for updated lifespan
    Updated SQL script to calculate lifespan up to 2024 and added ordering.

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,7 @@
--- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
-
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
-WHERE style LIKE '%Glam rock%' ;
+-- Lists all bands with Glam rock as their main style
+-- Longevity is calculated up to the year 2024
+-- Ordered by lifespan in descending order
+SELECT band_name, (IFNULL(split, 2024) - formed) AS lifespan
+    FROM metal_bands
+    WHERE style LIKE '%Glam rock%'
+    ORDER BY lifespan DESC;
