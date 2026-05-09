632e58210213210b6a25b1aaca4dc1287930ea2e
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-05-09 10:47:06 +0000

    Add Task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
-"""Provide stats about Nginx logs stored in MongoDB."""
+"""
+Provides some stats about Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def log_stats():
+    """
+    Displays stats about Nginx logs
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+    collection = client.logs.nginx
 
-    total_logs = nginx_collection.count_documents({})
+    # Ümumi logların sayını çap edirik
+    total_logs = collection.count_documents({})
     print(f"{total_logs} logs")
 
+    # Metodların statistikasını çap edirik
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        count = collection.count_documents({"method": method})
+        print(f"    method {method}: {count}")
 
-    status_check = nginx_collection.count_documents(
+    # Status check statistikasını çap edirik
+    status_check = collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
     print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
