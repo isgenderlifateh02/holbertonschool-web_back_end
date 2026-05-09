202faeca6101a50461fd893283d53b12d7527968
isgenderlifateh02 <isgenderlifateh02@gmail.com>
2026-05-09 13:21:26 +0400

    Update exercise.py

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,83 +1,26 @@
-#!/usr/bin/env python3
-''' Redis Module '''
-import redis
-import uuid
-from typing import Union, Callable, Optional
-from functools import wraps
-
-
-def count_calls(method: Callable) -> Callable:
-    ''' def count calls '''
-    @wraps(method)
-    def wrapper(self, *args, **kwds):
-        ''' def wrapper '''
-        key_m = method.__qualname__
-        self._redis.incr(key_m)
-        return method(self, *args, **kwds)
-    return wrapper
-
-
-def call_history(method: Callable) -> Callable:
-    ''' def call history '''
-    @wraps(method)
-    def wrapper(self, *args, **kwds):
-        ''' def wrapper'''
-        key_m = method.__qualname__
-        inp_m = key_m + ':inputs'
-        outp_m = key_m + ":outputs"
-        data = str(args)
-        self._redis.rpush(inp_m, data)
-        fin = method(self, *args, **kwds)
-        self._redis.rpush(outp_m, str(fin))
-        return fin
-    return wrapper
-
-
-def replay(func: Callable):
-    '''def replay'''
-    r = redis.Redis()
-    key_m = func.__qualname__
-    inp_m = r.lrange("{}:inputs".format(key_m), 0, -1)
-    outp_m = r.lrange("{}:outputs".format(key_m), 0, -1)
-    calls_number = len(inp_m)
-    times_str = 'times'
-    if calls_number == 1:
-        times_str = 'time'
-    fin = '{} was called {} {}:'.format(key_m, calls_number, times_str)
-    print(fin)
-    for k, v in zip(inp_m, outp_m):
-        fin = '{}(*{}) -> {}'.format(
-            key_m,
-            k.decode('utf-8'),
-            v.decode('utf-8')
-        )
-        print(fin)
-
-
-class Cache():
-    ''' class cache '''
-    def __init__(self):
-        ''' def init '''
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    @call_history
-    @count_calls
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        ''' def store '''
-        gen = str(uuid.uuid4())
-        self._redis.set(gen, data)
-        return gen
-
-    def get(self, key: str,
-            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        ''' def get '''
-        value = self._redis.get(key)
-        return value if not fn else fn(value)
-
-    def get_int(self, key):
-        return self.get(key, int)
-
-    def get_str(self, key):
-        value = self._redis.get(key)
-        return value.decode("utf-8")
+def replay(method):
+    """
+    Displays the history of calls of a particular function.
+    """
+    # Redis obyektinə metod vasitəsilə çatırıq
+    self_redis = method.__self__._redis
+    method_name = method.__qualname__
+    
+    # Giriş və çıxış açarlarını tapırıq
+    inputs_key = "{}:inputs".format(method_name)
+    outputs_key = "{}:outputs".format(method_name)
+    
+    # Redis-dən bütün tarixçəni çəkirik
+    inputs = self_redis.lrange(inputs_key, 0, -1)
+    outputs = self_redis.lrange(outputs_key, 0, -1)
+    
+    # Neçə dəfə çağırıldığını çap edirik
+    print("{} was called {} times:".format(method_name, len(inputs)))
+    
+    # inputs və outputs üzərində eyni anda dövr edirik
+    for inp, out in zip(inputs, outputs):
+        # Redis-dən gələn byte-ları decode edirik
+        input_str = inp.decode("utf-8")
+        output_str = out.decode("utf-8")
+        
+        print("{}(*({})) -> {}".format(method_name, input_str, output_str))
